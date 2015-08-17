1e18fbbe84ca052b0b9c064bf84412750d7c75b0
captiosus <royxu652@gmail.com>
2015-08-17 19:19:03 -0400

    model

--- app/models/riotapi.rb
+++ app/models/riotapi.rb
@@ -1,8 +1,8 @@
-require 'taric'
-require 'faraday'
-require 'typhoeus'
-require 'typhoeus/adapters/faraday'
 class Riotapi < ActiveRecord::Base
+  require 'taric'
+  require 'faraday'
+  require 'typhoeus'
+  require 'typhoeus/adapters/faraday'
   def new
     Taric.configure! do |config|
       config.api_key = 'e4d67b9c-85a6-444c-b285-a36e55a34bdd'
